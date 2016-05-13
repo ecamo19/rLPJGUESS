2cf396c03e1c453f16befd44eb6e073dbd403d93
Rasilgon <rasilgon@gmail.com>
2016-05-13 16:51:28 +0200

    Updated example file to meet new structure

--- Rlpj/examples/trying_Rlpj.R
+++ Rlpj/examples/trying_Rlpj.R
@@ -2,17 +2,25 @@
 library(Rlpj)
 
 # We need to specify the path of each input file inside the mainDir:
-file.co2<-"/co2_1901-2013_FAKE.txt"
-file.cru <- "/crudata/cru_1901_2006.bin"
-file.cru.misc <- "/crudata/cru_1901_2006misc.bin"
-file.ndep <- "/crudata/GlobalNitrogenDeposition.bin"
+file.co2<-"/home/trashtos/GitHub/lpjRun/crudata/co2_1901-2013_FAKE.txt"
+file.cru <- "/home/trashtos/GitHub/lpjRun/crudata/cru_1901_2006.bin"
+file.cru.misc <- "/home/trashtos/GitHub/lpjRun/crudata/cru_1901_2006misc.bin"
+file.ndep <- "/home/trashtos/GitHub/lpjRun/crudata/GlobalNitrogenDeposition.bin"
 
 # if you are using the global_cf.ins file you neet to specify the site specific input files as well
-file.temp <- "/inputLPJ/temp.nc"
-file.prec <- "/inputLPJ/prec.nc"
-file.insol <- "/inputLPJ/rad.nc"
+file.temp <- "/home/trashtos/GitHub/lpjRun/inputLPJ/temp.nc"
+file.prec <- "/home/trashtos/GitHub/lpjRun/inputLPJ/prec.nc"
+file.insol <- "/home/trashtos/GitHub/lpjRun/inputLPJ/rad.nc"
 mainDir <- "/home/trashtos/GitHub/lpjRun"
 gridList <- "gridlist_geb.txt"
+
+result <-   runLPJ(mainDir, gridList, scale = "global",mode = "cf", file.co2,
+                   file.cru, file.cru.misc, file.ndep, file.temp, file.prec,
+                   file.insol, delete = F)
+
+
+
+
 
 
 # Create some paramaters to test modell.
@@ -21,7 +29,7 @@ parameterDefault <- list (run_lamda_max = NULL, run_reprfrac=NULL, run_emax = NU
                           run_wscal_min = NULL, run_drought_tolerance=NULL)
 # I want to test 6 different values for emax.
 # I want therefore to run 20 time the LPJ
-par <- seq(1,5, len = 6)
+par <- seq(1,5, len = 3)
 print (par)
 # I create the list object with the parameters
 parameterList <- vector("list", length(par))
@@ -30,15 +38,18 @@ for (i in 1:length(par)) {
   parameterList[[i]] <- parameterDefault
 }
 #print(parameterList[c(2:3)])
+mySetup  <- setupLPJParallel(numCores = 3, clusterType ="SOCK", mainDir = "/home/trashtos/GitHub/lpjRun")
+
+proc1 <- proc.time()
+result <-   runLPJ(mainDir, gridList, scale = "global", mode = "cf", file.co2,
+                   file.cru, file.cru.misc, file.ndep, file.temp, file.prec,
+                   file.insol, parameterList = parameterList,  parallel = TRUE,
+                   processing = TRUE, setupObject = mySetup, delete = TRUE)
+proc.time() - proc1
 
 
 # call library for setup object
-suspect  <- setupParallel(3, "SOCK", "global", "cf", gridList = "gridlist_geb.txt", mainDir = "/home/trashtos/GitHub/lpjRun")
 
-# run the modell
-proc1 <- proc.time()
-result <- runLPJParallel(setupObject = suspect, plot.data = FALSE, save.plots = FALSE,
-               parameterList=parameterList, file.co2, file.cru, file.cru.misc,
-               file.ndep, file.temp, file.prec, file.insol)
 
-proc.time() - proc1
+
+
