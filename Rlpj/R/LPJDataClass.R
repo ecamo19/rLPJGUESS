fb5cb5ac67c65333283bc94c9240afdfa4248e97
Rasilgon <rasilgon@gmail.com>
2016-05-23 22:11:22 +0200

    Added subsetting, get, show and initialize methods

--- Rlpj/R/LPJDataClass.R
+++ Rlpj/R/LPJDataClass.R
@@ -2,24 +2,81 @@
 #' @description  This is a data class object to store both the LPJ-GUESS outputs
 #' and the information that was passed to LPJ-GUESS.
 #' @return a data class object with the slots runInfo and dataTypes
-#' @export
 #' @keywords Rlpj
 #' @author Ramiro Silveyra Gonzalez
 setClass("LPJData",
          representation(
            runInfo = "list",
            dataTypes = "list"
-           )
+           ),
+         prototype (
+           runInfo= list(),
+           dataTypes = list()
+         )
 )
 
-LPJData <- function(runInfo=list(run = "This is run one", parameters = "XX paramereters used"),
-                    dataTypes = list(aaet = "aaet",  agpp = "agpp")
-                      )
-  new("LPJData", runInfo=runInfo,  dataTypes=dataTypes)
+setMethod(f= "initialize",
+           signature="LPJData",
+           definition= function(.Object,
+                                runInfo=list(run = "This is run one", parameters = "XX paramereters used"),
+                                dataTypes = list(par = "parameters")){
+            .Object@runInfo <- runInfo
+            .Object@dataTypes <- dataTypes
+             # validaditate stuff
+             #validObject(.Object)
+             return(.Object)
+             }
+)
+
 
 #Defining slot setters1!
-setGeneric("runInfo<-", function(x, value) standardGeneric("runInfo<-"))
-setReplaceMethod("runInfo", "LPJData", function(x, value) {x@runInfo <- value; validObject(x); x})
+setGeneric("runInfo<-", function(object, value) standardGeneric("runInfo<-"))
+setReplaceMethod(f ="runInfo",
+                 signature = "LPJData",
+                 definition = function(object, value){
+                   object@runInfo <- value
+                   #validObject(object); object
+                   }
+                 )
+setGeneric("dataTypes<-", function(object, value) standardGeneric("dataTypes<-"))
+setReplaceMethod(f = "dataTypes",
+                 signature ="LPJData",
+                 definition = function(object, value){
+                   object@dataTypes <- value
+                   #validObject(object); object
+                   }
+                 )
 
-setGeneric("dataTypes<-", function(x, value) standardGeneric("dataTypes<-"))
-setReplaceMethod("dataTypes", "LPJData", function(x, value) {x@dataTypes <- value; validObject(x); x})
+setMethod(f ="[[",
+          signature ="LPJData",
+          definition = function(x,i,j,drop){
+            if(i=="runInfo"){return(x@runInfo)}else {}
+            if(i=="dataTypes"){return(x@dataTypes)}else {}
+          }
+          )
+
+
+setGeneric("getDataTypes",
+           function(object){
+             standardGeneric("getDataTypes")
+             }
+           )
+
+setMethod(f= "getDataTypes",
+          signature ="LPJData",
+          definition = function(object){
+            return(object@dataTypes)
+            }
+          )
+
+setMethod (f= "show",
+             signature ="LPJData",
+             function(object){
+                   cat("class              : LPJData\n")
+                   cat("LPJ template 1     : ");cat(object@runInfo$template1); cat("\n")
+                   cat("grid cells         : ");cat(object@runInfo$gridList); cat("\n")
+                   cat("LPJ model outputs  : "); cat(length(names(object@dataTypes)));cat(" outputs\n")
+                   cat(names(object@dataTypes), sep = "\t", fill = TRUE)
+               }
+  )
+
