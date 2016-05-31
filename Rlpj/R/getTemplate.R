ec62a4d8efb36f381de8194f468035584adbe30f
Rasilgon <rasilgon@gmail.com>
2016-05-31 17:06:48 +0200

    Moved function to single files. Edited help

--- Rlpj/R/getTemplate.R
+++ Rlpj/R/getTemplate.R
@@ -1,10 +1,9 @@
 #' @title A get template function
 #' @description  This function returns the LPJ-GUESS templates required for the
-#'  writeTemplate function.
-#' It also allows users to download the templates.
+#'  writeTemplate function. It also allows users to download the templates.
 #' @param type a character string indicating the template name: global,
-#'  global_cf, global_cru, europe, europe_cf or europe_cru
-#' @param outputDir a character string indicating path to the output directory
+#'  global_cf, global_cru, europe, europe_cf, europe_cru
+#' @param outputDir a character string indicating path to the output directory  (optional)
 #' @return a template object or template.ins file in the specified folder
 #' @export
 #' @keywords Rlpj
@@ -16,7 +15,7 @@
 getTemplate <- function(type = NULL, outputDir = NULL){
   if (is.null(type) || type != "global" & type != "global_cf" & type != "global_cru"
       &  type != "europe" &  type != "europe_cf" &  type != "europe_cru"){
-    stop("Cannot recognize the template: global?? europe??")
+    stop("Invalid template type")
   }
   tmp <- templates[[strsplit(type, "_")[[1]][1] ]][[type]]
   if(is.null(tmp)){
@@ -29,26 +28,3 @@ getTemplate <- function(type = NULL, outputDir = NULL){
     return(paste(type, ".ins", sep = ""))
   }
 }
-
-
-#' @title A get paremeter list function
-#' @description  This function returns the LPJ-GUESS parameter list required for
-#'  the writeTemplate function.It also allows to see the default values of the templates.
-#' @param scale a character string indicating whether the parameters are for the
-#' global or europe template
-#' @return a list with parameter names and their values
-#' @export
-#' @examples \dontrun{
-#' parameterList <- getParameterList("global")
-#' }
-getParameterList <- function(scale = NULL){
-  if ( is.null(scale) || scale != "global" & scale != "europe"){
-    stop("Please provide a valid scale: global or europe")
-  }
-  tmp <- parameterList.default [[scale]]
-  return(tmp)
-}
-
-
-
-
