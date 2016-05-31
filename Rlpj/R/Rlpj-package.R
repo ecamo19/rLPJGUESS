b61a5a4a18409c8250310b7f316f7863a0870c5e
Rasilgon <rasilgon@gmail.com>
2016-05-31 17:05:12 +0200

    Imports. Included other functions in overview. Typos

--- Rlpj/R/Rlpj-package.R
+++ Rlpj/R/Rlpj-package.R
@@ -4,11 +4,12 @@
 #' It also allows to parallelize the execution of LPJ-Guess on personal laptops
 #' and on HPC.
 #'
-#' The package should be particularly useful for users aiming at integrating LPj outputs
-#' in R routines and parellizing LPJ-Guess.
+#' The package should be particularly useful for users aiming at integrating LPJ outputs
+#' in R routines and parellizing the model.
 #'
 #' Below is a list of the functions grouped by theme. See the vignette for more
-#' information and some examples.
+#' information and some examples   (you can open it by running this command:
+#' \code{vignette('Rlpj')})
 #'
 #' @section I. Run LPJ-Guess:  \itemize{
 #'   \item \code{\link{runLPJ}} To run LPJ-Guess serial/parallel
@@ -17,16 +18,29 @@
 #' }
 #'
 #' @section II. Visualize data: \itemize{
-#'   \item \code{\link{plotData}} To plot data from LPJData
+#'   \item \code{\link{plotLPJData}} To plot data from LPJData objects
 #' }
+#'
 #' @section III. Utility functions: \itemize{
 #'  \item \code{\link{getTemplate}} To obtain the in-package stored model templates
 #'  \item \code{\link{getParameterList}} To obtain the in-package stored parameter default values
 #' }
 #'
+#' @section IV. Other functions: \itemize{
+#'  \item \code{\link{callLPJ}} To make a system call for LPJ-GUESS
+#'  \item \code{\link{getData}} To read and process LPJ-GUESS outputs
+#'  \item \code{\link{writeTemplate}} To write LPJ-GUESS templates
+#'
+#' }
 #'
 #' @author Except where indicated otherwise, the functions in this package were
 #'  written by Ramiro Silveyra Gonzalez, Maurizio Bagnara and Florian Hartig
+
+#' @keywords package
+#' @keywords LPJ-Guess
+#' @import methods
+#' @import zoo
+#' @import snow
 
 "_PACKAGE"
 
