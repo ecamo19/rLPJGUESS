b619d66ef4f42b9150110dd99c4a466ce622fde0
Rasilgon <rasilgon@gmail.com>
2016-05-31 17:15:52 +0200

    Included option for activating PFTs and species

--- Rlpj/R/runLPJWrapper.R
+++ Rlpj/R/runLPJWrapper.R
@@ -2,13 +2,13 @@
 #
 # @description This function modifies the lpj template with the desired parameter
 #  values and then does a system call for running the lpj modell.
-#  The modell outputs are processed and plotted, if specified.
+#  The modell outputs are processed, plotted (if set to TRUE)
 # @param runObject a named list created by the runLPJParallel function containing
 # the following: mainDir, template1, template2, gridList, runDir, outDir, mode,
 # scale, typeList, runID,  gridFilename, plot.data and save.plots
-# @return a data class object with the slots runInfo and dataTypes. The runInfo
+# @return a a data class object with the slots runInfo and dataTypes. The runInfo
 # slot contains the provided run information and parameters; the dataTypes holds
-# the processed output data from the run
+# the processed output data from the run.
 # @keywords Rlpj
 # @author Florian Hartig, Ramiro Silveyra Gonzalez, Maurizio Bagnara
 # @note Based an older code of Istem Fer, Uni Potsdam
@@ -21,33 +21,33 @@ runLPJWrapper <- function(runObject){
   #----------------------------------------------------------------------------#
   # checking conditions
   if (is.null(runObject[["runDir"]]) || !file.exists(runObject$runDir)){
-    stop("Please provide a valid run directory.")
+    stop("Please provide a valid run directory")
   }
   if (is.null(runObject[["outDir"]]) || !file.exists(runObject$outDir)){
-    stop("Please provide a valid output directory.")
+    stop("Please provide a valid output directory")
   }
   if (is.null(runObject[["template1"]])){
-    stop("Please provide a valid  template1 name.")
+    stop("Please provide a valid  template1 name")
   }
   if (is.null(runObject[["template2"]])){
-    stop("Please provide a valid  template2 name.")
+    stop("Please provide a valid  template2 name")
   }
   #if (is.null(runObject[["parameterList"]])){ No because it could run with default values
   #  stop("Please provide a valid parameter list.")
   #}
   if (is.null(runObject[["plot.data"]])){
-    warning("The plot.data boolean has not been provided. It will be set to FALSE.")
+    warning("The plot.data boolean has not been provided. It will be set to FALSE")
     runObject$plot.data <- FALSE
     runObject$save.plots <- FALSE
   }
   if ( is.null(runObject[["save.plots"]])){
-    warning("The save.plots boolean has not been provided. It will be set to FALSE.")
+    warning("The save.plots boolean has not been provided. It will be set to FALSE")
     runObject$save.plots <- FALSE
   }
   if (is.null(runObject[["typeList"]])){
     runObject$typeList <- typelist.default
     warning("The output type list has not been provided")
-    warning("Setting type list to default values.")
+    warning("Setting type list to default values")
   }
   #----------------------------------------------------------------------------#
   # WRAP THE FUNCTIONS
@@ -59,16 +59,16 @@ runLPJWrapper <- function(runObject){
   # set the wd
   setwd(runObject$runDir)
   # write out files
-  writeLines(runObject$template1,file.path(runObject$runDir,runObject$template1Name))
-  writeLines(runObject$template2,file.path(runObject$runDir,runObject$template2Name))
-  writeLines(runObject$gridList, file.path(runObject$runDir, runObject$gridListName))
+  writeLines(runObject$template1Mem,file.path(runObject$runDir,runObject$template1))
+  writeLines(runObject$template2Mem,file.path(runObject$runDir,runObject$template2))
+  writeLines(runObject$gridListMem, file.path(runObject$runDir, runObject$gridList))
   # writing template
-  writeTemplate(runObject$template1Name, runObject$parameterList,
+  writeTemplate(runObject$template1, runObject$parameterList,
                   runObject$runDir)
   # calling the model
-  callLPJ(runObject$mainDir, runObject$runDir,runObject$template2Name, runObject$mode)
+  callLPJ(runObject$mainDir, runObject$runDir,runObject$template2, runObject$mode)
   # getting data
-  LPJout <- getData(runObject$typeList, runObject$outDir, runObject,
+  LPJout <- getData(runObject$outDir, runObject$typeList, runObject,
                      runObject$processing)
 
 
@@ -91,7 +91,7 @@ runLPJWrapper <- function(runObject){
   save(runObject, file = file.path(runObject$runInfoDir,
                                    paste("runInfo", runObject$runID,".Rdata", sep = "")))
   if (runObject[["plot.data"]] == TRUE){
-    plotData(data = LPJout@dataTypes, typeList = runObject$typeList,
+    plotLPJData(x = LPJout, typeList = runObject$typeList,
              outDir = runObject$outDir, save.plots = runObject$save.plots,
              prefix = paste("run",runObject$runID, "_", sep=""))
   }
