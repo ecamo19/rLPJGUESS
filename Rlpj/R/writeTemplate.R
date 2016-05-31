f37a458087c978c1579ffe5755211619133a5c86
Rasilgon <rasilgon@gmail.com>
2016-05-31 17:18:17 +0200

    Included class checks. Edited help

--- Rlpj/R/writeTemplate.R
+++ Rlpj/R/writeTemplate.R
@@ -1,13 +1,18 @@
 #' @title A writing template function for LPJ
-#' @description  This function reads the LPJ template, and replaces parameters within
-#' it based on the provided parameters list. Not provived  parameter, will set it
-#' to the default values. The function assumes that a copy of the template
-#'  is already placed in the folder (runDir).
+#' @description  This function reads a template, and replaces parameters with the
+#' provides parameters list. If any parameter values is not provided, the function will
+#' set it to the default values. The function assumes that a copy of the template
+#'  is already placed in the run directory.
 #' @param template1 a character string providing the general model template,
 #'  e.g, global.ins. Provide only the file name, not the path
 #' @param parameterList  a named list containing the parameters to be calibrated
 #' @param runDir a character string indicating path to the run directory
-#' @return a modified template that is written out in the run directory
+#' @return none
+#' @details The provided template can be either the one provided by the package or
+#' a self edited template. The function assumes a specific coding for writing the
+#' parameters values. For this reason, we recommend to use the package templates.
+#' If using self edited templates, please take the package templates as a reference.
+#' @seealso \code{\link{getTemplate}}, \code{\link{getParameterList}}
 #' @export
 #' @keywords Rlpj
 #' @author Florian Hartig, Ramiro Silveyra Gonzalez, Maurizio Bagnara
@@ -20,10 +25,13 @@ writeTemplate <- function(template1 = NULL, parameterList = NULL, runDir = NULL)
 
   # Checking provided parameters
   if (is.null(runDir) || !file.exists(runDir) ){
-    stop("Please provide a valid run directory.")
+    stop("Please provide a valid run directory")
   }
   if (is.null(template1) || !file.exists(file.path(runDir, template1))){
-    stop("The provided template  does not exits. Please provide a template name.")
+    stop("The provided template  does not exits. Please provide a template name")
+  }
+  if (is.null(parameterList) || !class(parameterList) =="list"){
+    stop("Please provide a parameterList")
   }
   # call the function
   if (grepl("global",template1)){
@@ -36,7 +44,7 @@ writeTemplate <- function(template1 = NULL, parameterList = NULL, runDir = NULL)
       # Exceptions are the fine roots distributions for all species and PFTs
       parameterList <- checkParameters(scale = "europe", parameterList)
   }else{
-    stop("writeParameters: Cannot recognize the template: neither global nor europe")
+    stop("Cannot recognize the template: neither global nor europe")
   }
   # getting parameters names
   parameterNames <- names(parameterList)
