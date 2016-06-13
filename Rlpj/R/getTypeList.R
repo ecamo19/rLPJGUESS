ddf9a9103558118c6f4c1488783baaaa1e3709a0
Rasilgon <rasilgon@gmail.com>
2016-06-13 10:31:46 +0200

    added authours

--- Rlpj/R/getTypeList.R
+++ Rlpj/R/getTypeList.R
@@ -2,6 +2,7 @@
 #' @description  This function returns the LPJ-GUESS output type list
 #' @return a character vector with the default output types
 #' @export
+#' @author Ramiro Silveyra Gonzalez, Maurizio Bagnara
 #' @examples \dontrun{
 #' typelist <- getTypeList()
 #' }
