3a28b80031992070b2565dec8cf5f08376930a07
Rasilgon <rasilgon@gmail.com>
2016-06-13 10:37:14 +0200

    Typos

--- Rlpj/R/setupLPJParallel.R
+++ Rlpj/R/setupLPJParallel.R
@@ -1,22 +1,22 @@
 #' @title The function to create a setup for parallel runs of the LPJ-GUESS
 #'
-#' @description This function first creates a setup for running the LPJ in parallel
+#' @description This function first creates a setup for running the LPJ-GUESS in parallel
 #'  based on the provided input parameters. The function assumes a specific initial
 #'  configuration. A folder (mainDir) containing the model templates for LPJ-GUESS
-#'  (optional) and link to the model executable (not for Windows).
+#'  (optional) and link to the model executable.
 #' @param numCores  a integer specifying number of cores of the cluster
 #' @param clusterType a character string indicating the type of cluster to be created.
 #'  If running on personal computer, type should be SOCK. If running on HPC,
 #'  type should be MPI
 #' @param mainDir a character string indicating the path to the directory where
-#'  the template and the model link are located and in which the function will create
+#'  the template and the model link are located, and in which the function will create
 #'  the directory structure for the outputs
-#' @return a LPJSetup object
+#' @return an LPJSetup object
 #' @seealso  \url{https://cran.r-project.org/web/packages/Rmpi/Rmpi.pdf},
 #'  \url{https://cran.r-project.org/web/packages/snow/snow.pdf}
 #' @export
 #' @keywords Rlpj
-#' @author Florian Hartig, Ramiro Silveyra Gonzalez, Maurizio Bagnara
+#' @author  Ramiro Silveyra Gonzalez, Maurizio Bagnara, Florian Hartig
 #' @examples \dontrun{
 #' mainDir <- "/some/absolute/path/mainDir"
 #' list.files(mainDir)
@@ -43,20 +43,20 @@ setupLPJParallel <- function(numCores=NULL, clusterType = NULL, mainDir=NULL)
   #----------------------------------------------------------------------------#
   # number of cores
   if (is.null(numCores) | (typeof(numCores) != "double") ){
-    stop( "Please provide a valid number of cores.")
+    stop( "Please provide a valid number of cores")
   }
   # cluster type
   if (is.null(clusterType) || clusterType != "SOCK" & clusterType != "MPI" ){
-    stop("Please provide a valid cluster type: SOCK or MPI.")
+    stop("Please provide a valid cluster type: SOCK or MPI")
   }
   if (clusterType == "MPI"){
     if (!requireNamespace("Rmpi", quietly = TRUE)){
-      stop("Can't load required library 'Rmpi', runLPJparallel will now exit.")
+      stop("Can't load required library 'Rmpi', runLPJparallel will now exit")
     }else{
       # check cluster size
       numCores.available <- Rmpi::mpi.universe.size() - 1
       if ( numCores.available == 0) {
-        stop("\nThere are not enough available cores  to create a cluster.")
+        stop("There are not enough available cores  to create a cluster")
       }else if ( numCores.available != numCores) {
         message(paste("There are", numCores.available,"cores available. ", sep = " "))
         message(paste("You requested", numCores,  "cores.", sep = " "))
@@ -67,7 +67,7 @@ setupLPJParallel <- function(numCores=NULL, clusterType = NULL, mainDir=NULL)
   }
   # mainDir
   if (is.null(mainDir) || !file.exists(mainDir)){
-    stop("Please provide a valid main directory.")
+    stop("Please provide a valid main directory")
   }
   #----------------------------------------------------------------------------#
   # CREATE THE FOLDER STRUCTURE
