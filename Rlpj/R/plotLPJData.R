a10462b182942090bde1605cdcd29ccce701c359
Rasilgon <rasilgon@gmail.com>
2016-07-05 09:46:02 +0200

    Simplified code, plot sorted by variable names

--- Rlpj/R/plotLPJData.R
+++ Rlpj/R/plotLPJData.R
@@ -39,7 +39,8 @@ plotLPJData <- function(x=NULL, typeList = NULL, outDir= NULL,
 
   data <- x@dataTypes
   # Plot from
-  typeList.available <- names(data)
+  typeList.available <- sort(names(data))
+
   if (is.null(typeList) || !class(typeList) == "character"){
 
     cat("\nNo typeList has been provided")
@@ -79,47 +80,33 @@ plotLPJData <- function(x=NULL, typeList = NULL, outDir= NULL,
         #keep <- rep(TRUE, length(coord))
         #sub_data <- coord[coord>=min(lon.extent) & Lon<=max(lon.extent) & Lat <=max(lat.extent) & Lat>=min(lat.extent))
         for (k in 1:length(coordinates)){
-          df_subset <- df[df$Lat==coordinates[[k]][1] & df$Lon==coordinates[[k]][2],]
-          if(zoo::is.zoo(df_subset) == FALSE){
-            df_subset <- convertTS(df_subset)
-          }
+          values <- df[df$Lat==coordinates[[k]][1] & df$Lon==coordinates[[k]][2],]
+          values <- convertTS(values)
           if (save.plots){
             pdf(file.path(outDir, paste(prefix, typeList.valid[[i]], ".pdf", sep="")))#width=1000,height=750
-            if(length(colnames(df_subset))==1){
-              plot(df_subset, main =paste("Grid", coordinates[[k]][1], coordinates[[k]][2],
+            if(length(colnames(values))==1){
+              plot(values, main =paste("Grid", coordinates[[k]][1], coordinates[[k]][2],
                                           "Variable:", typeList.valid[[i]]),xlab="Years", ylab="NULL")
             }else{
-              plot(df_subset, main =paste("Grid", coordinates[[k]][1], coordinates[[k]][2],
+              plot(values, main =paste("Grid", coordinates[[k]][1], coordinates[[k]][2],
                                           "Variable:", typeList.valid[[i]]),xlab="Years")
             }
             dev.off()
           }else{
-            if(length(colnames(df_subset))==1){
-              plot(df_subset,  main =paste("Grid", coordinates[[k]][1], coordinates[[k]][2],
-                                           "Variable:", typeList.valid[[i]]),xlab="Years", ylab="NULL")
-            }else{
-              plot(df_subset,  main =paste("Grid", coordinates[[k]][1], coordinates[[k]][2],
+            plot(values,  main =paste("Grid", coordinates[[k]][1], coordinates[[k]][2],
                                            "Variable:", typeList.valid[[i]]),xlab="Years")
-            }
           }
         }
 
     }else{
+      values <- df
     # something like is zoo
       if (save.plots){
         pdf(file.path(outDir, paste(prefix, typeList.valid[[i]], ".pdf", sep="")))#width=1000,height=750
-        if(length(colnames(df))==1){
-          plot(df, main =paste("Variable:", typeList.valid[[i]]),xlab="Years", ylab="NULL")
-        }else{
-          plot(df, main =paste("Variable:", typeList.valid[[i]]),xlab="Years")
-        }
+        plot(values, main =paste("Variable:", typeList.valid[[i]]),xlab="Years")
         dev.off()
       }else{
-        if(length(colnames(df))==1){
-          plot(df,  main =paste("Variable:", typeList.valid[[i]]),xlab="Years", ylab="NULL")
-        }else{
-          plot(df,  main =paste("Variable:", typeList.valid[[i]]),xlab="Years")
-        }
+        plot(values,  main =paste("Variable:", typeList.valid[[i]]),xlab="Years", ylab="NULL")
       }
     }
   }
