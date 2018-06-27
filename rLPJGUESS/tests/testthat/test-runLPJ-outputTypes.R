c93af12896233333ce440eed7b452915ffe87b28
Florian Hartig <florian.hartig@biologie.uni-regensburg.de>
2018-06-27 16:05:06 +0200

    basic tests

--- rLPJGUESS/tests/testthat/test-runLPJ-outputTypes.R
+++ rLPJGUESS/tests/testthat/test-runLPJ-outputTypes.R
@@ -1,76 +1,56 @@
-context("Test basic")
+testthat::context("Test basic")
 set.seed(1)
 library(rLPJGUESS)
 
-# Here place all the general test that don't require to evaluate the model code
-mainDir <- normalizePath("~/Github/TestCode/LPJrunTest/")
+testthat::test_that("basic run", {
 
-# We need to specify the path of each input file inside the mainDir:
-file.co2<-normalizePath("~/Github/TestCode/LPJrunTest/crudata/co2_1765-2500_RCP3.txt")
-file.cru <- normalizePath("~/Github/TestCode/LPJrunTest/crudata/cru_1901_2006.bin")
-file.cru.misc <- normalizePath("~/Github/TestCode/LPJrunTest/crudata/cru_1901_2006misc.bin")
-file.ndep <- normalizePath("~/Github/TestCode/LPJrunTest/crudata/GlobalNitrogenDeposition.bin")
-# if you are using the global_cf.ins file you neet to specify the site specific input files as well
-file.temp <- normalizePath("~/Github/TestCode/LPJrunTest/temp.nc")
-file.prec <- normalizePath("~/Github/TestCode/LPJrunTest/prec.nc")
-file.insol <- normalizePath("~/Github/TestCode/LPJrunTest/rad.nc")
-variable.temp <- "temp"
-variable.prec <-"prec"
-variable.insol <- "rad"
+  file.co2<-"/some/absolute/path/crudata/co2_1901-2013.txt"
+  file.cru <- "/some/absolute/path/crudata/cru_1901_2006.bin"
+  file.cru.misc <- "/some/absolute/path/crudata/cru_1901_2006misc.bin"
+  file.ndep <- "/some/absolute/path/crudata/GlobalNitrogenDeposition.bin"
+  file.temp <- "/some/absolute/path/cfdata/temp.nc"
+  file.prec <- "/some/absolute/path/cfdata/prec.nc"
+  file.insol <- "/some/absolute/path/cfdata/rad.nc"
 
-gridList <- "gridlist.txt"
-toremove <- list.files(mainDir,full.names = T, recursive = T,  pattern = "run")
-do.call(file.remove, list(toremove))
-settings <- list (gridList = gridList,mode = "cf", scale = "europe",
-                  file.co2 = file.co2, file.cru = file.cru, file.cru.misc = file.cru.misc,
-                  file.ndep = file.ndep, file.temp = file.temp, file.prec = file.prec,
-                  file.insol = file.insol, variable.temp = variable.temp, variable.prec = variable.prec,
-                  variable.insol = variable.insol,save = F, delete = T, plot.data =F,
-                  save.plots=F, processing = F)
+  mainDir <- "../LPJ-Example"
+  list.files(mainDir)
 
+  settings <- list (gridList = NA ,mode = "cf", scale = "global",
+                    file.co2 = file.co2, file.cru = file.cru,
+                    file.cru.misc = file.cru.misc, file.ndep = file.ndep,
+                    file.temp = file.temp, file.prec = file.prec,
+                    file.insol = file.insol, delete = F)
 
-# Settings to test
-settingName <-c("nyear_spinup", "iffire", "npatch", "patcharea", "estinterval",
-                "ifdisturb", "distinterval" )
-settingName <- paste("run", settingName, sep = "_")
-settingValue <-c(250, 0, 5, 500, 5, 0, 50 )
-
-
-checkTemplates <- function(testTemplate, runTemplate, setting){
-  setting <- gsub("run_", "", setting)
-  expect_true(identical( testTemplate[grepl(setting, testTemplate)],  runTemplate[grepl(setting, runTemplate)]))
-}
-
-test_that("templates are the same", {
-
-  skip_on_travis()
-  skip_on_cran()
-
-for (i in 1:length(settingName)){
-    designLPJ <- getDesign("europe", list = T)
-    designLPJ[[settingName[i]]] <- settingValue[i]
-    settings$design <- designLPJ
-    # runLPJ
-    result <-   runLPJ(mainDir, settings= settings)
-    # get the folder with test data
-    examplesDir <- list.dirs(system.file("extdata", package = "rLPJGUESS"), recursive = T)
-    examplesDir <- examplesDir[grepl(settingName[i],examplesDir )]
-    examplesFiles <- list.files(examplesDir, full.names = T, recursive = T)
-    # test whether the templates are the same
-    testTemplate1 <- readLines(examplesFiles[grepl("europe.ins",examplesFiles)])
-    runTemplate1 <- result["runInfo"]$template1Mem
-   # checkTemplates(testTemplate1, runTemplate1, settingName[i])
-    #testTemplate2 <- readLines(examplesFiles[grepl("europe_cf.ins",examplesFiles)])
-    #runTemplate2 <- result["runInfo"]$template2Mem
-    #checkTemplates(testTemplate2, runTemplate2, settingName[i])
-
-    # read in the templates for the example
-
-    # test whether the ooutpus are the same
-
-
-  }
+  # result <-  runLPJ(mainDir, settings= settings)
 
 })
+
+
+testthat::test_that("data test", {
+
+  data <- "../LPJ-Example/runDirectory/outDirectory"
+
+  LPJout <- getLPJData(typeList = c("cmass",  "lai"),
+                        data, runInfo = list(parameter1 = 0.5, grid = 1))
+  plotLPJData(LPJout, typeList = c("cmass", "lai"))
+})
+
+
+
+# getTemplate To obtain the in-package stored model templates
+#
+# getParameterList To obtain the parameter default values
+#
+# getDesign To obtain the default desgin
+#
+# getTypeList To obtain the default output model types
+#
+# getRunInfo To recover data or parameters from the runInfoDir
+#
+#
+#
+# writeTemplate To write LPJ-GUESS templates
+
+
 
 
