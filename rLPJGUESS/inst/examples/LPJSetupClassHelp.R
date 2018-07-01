05c7c28d27839778a5b58864573826f98e7e0d3d
Rasilgon <rasilgon@gmail.com>
2018-07-01 13:37:22 +0200

    Provided context to example

--- rLPJGUESS/inst/examples/LPJSetupClassHelp.R
+++ rLPJGUESS/inst/examples/LPJSetupClassHelp.R
@@ -1,5 +1,19 @@
 \dontrun{
-LPJsetup
+# Create a parallel setup for MPI cluster
+mySetup <- setupLPJParallel(numCores= 20, clusterType = "MPI", mainDir=mainDir)
 
-LPJsetup["numCores"]
+# Create a parallel setup for SOCK cluster
+mySetup <- setupLPJParallel(numCores= 3, clusterType = "SOCK", mainDir=mainDir)
+
+# Run LPJ-GUESS in parallel
+result <-   runLPJ(x = mySetup,  settings= settings, parameterList = parameterList )
+
+
+# Accessing information
+mySetup["clusterType"]
+mySetup["numCores"]
+mySetup["mainDir"]
+mySetup["runDir"]
+mySetup["outDir"]
+
 }
