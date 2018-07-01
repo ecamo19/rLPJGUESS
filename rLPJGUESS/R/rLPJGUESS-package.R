5dc30f57b042a84f2d40de12c0c25dd737bb55c8
Rasilgon <rasilgon@gmail.com>
2018-07-01 21:46:18 +0200

    Improved help. Typos. Include section for the s4 classes

--- rLPJGUESS/R/rLPJGUESS-package.R
+++ rLPJGUESS/R/rLPJGUESS-package.R
@@ -26,7 +26,7 @@
 #'  \item \code{\link{getParameterList}} To obtain the parameter default values
 #'  \item \code{\link{getDesign}} To obtain the default desgin
 #'  \item \code{\link{getTypeList}} To obtain the default output model types
-#'  \item \code{\link{getRunInfo}} To recover data or parameters from the runInfoDir
+#'  \item \code{\link{getRunInfo}} To recover data or parameters from the runInfo file
 #' }
 #'
 #' @section IV. Other functions: \itemize{
@@ -35,6 +35,13 @@
 #'  \item \code{\link{writeTemplate}} To write LPJ-GUESS templates
 #'
 #' }
+#' @section V. S4 classes: \itemize{
+#'  \item \linkS4class{LPJData} To store both LPJ-GUESS outputs
+#' and run information
+#'  \item  \linkS4class{LPJSetup} To store the configuration for parallel
+#' runs
+#' }
+#'
 #'
 #' @author Except where indicated otherwise, the functions in this package were
 #'  written by Ramiro Silveyra Gonzalez, Maurizio Bagnara and Florian Hartig
