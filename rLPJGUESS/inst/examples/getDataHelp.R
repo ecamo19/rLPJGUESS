bd9c227bbf429f98ef7a5038ce94ccf6818a321e
Rasilgon <rasilgon@gmail.com>
2018-07-01 12:39:20 +0200

    Provided context to example

--- rLPJGUESS/inst/examples/getDataHelp.R
+++ rLPJGUESS/inst/examples/getDataHelp.R
@@ -1,4 +1,20 @@
 \dontrun{
-LPJout <- getLPJData( typeList = c("aaet",  "cflux","lai", "nflux"),
-           "~/path/to/output/files", runInfo = list(parameter1 = 0.5, grid = 1))
-}
+#  Specify folder where the ASCII outputs produced by LPJ-GUESS files are located
+outDir <- "/some/absolute/path/runDirectory/outDirectory"
+list.files(outDir)
+
+# Any specific output you are interest in
+typeList <- c("aaet","lai")
+
+# What information should be associated to the data
+runInfo <-  list(parameters = 0.5, grid = 1)
+
+# Call the function and obtain the outputs as LPJData and
+  # stored as zoo time series
+LPJout <- getLPJData(x = outDir, typeList = typeList, runInfo = runInfo, processing = TRUE)
+  # stored as data.frame
+LPJout <- getLPJData(x = outDir, typeList = typeList, runInfo = runInfo, processing = FALSE)
+
+# Plot the data
+plotLPJData(LPJout)
+  }
