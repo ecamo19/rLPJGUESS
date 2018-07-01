8154fb7f498d428912c20e8c5f60c49624e2f090
Rasilgon <rasilgon@gmail.com>
2018-07-01 19:50:33 +0200

    Provided context to example

--- rLPJGUESS/inst/examples/getParameterListHelp.R
+++ rLPJGUESS/inst/examples/getParameterListHelp.R
@@ -1,10 +1,21 @@
 \dontrun{
+# Get the default parameters as a list
 parameterList <- getParameterList("global", list = TRUE)
-head(parameterList)
-tail(parameterList)
+parameterList <- getParameterList("europe", list = TRUE)
+
+# Modify any parameter
+parameterList$common_emax <- 1
+parameterList[["common_emax"]] <- 1
 
 
+# Get the default parameters as a matrix
 parameterList <- getParameterList("global", list = FALSE)
-head(parameterList)
-tail(parameterList)
+parameterList <- getParameterList("europe", list = FALSE)
+
+# Modify any parameter
+parameterList["common_emax",] <- 1
+
+# Call runLPJ with the desired parameter
+result <-  runLPJ(x=mainDir, settings=settings, parameterList = parameterList)
+
 }
