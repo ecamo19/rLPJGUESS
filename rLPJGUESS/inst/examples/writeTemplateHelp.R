4b7a393dc6d880d8ee7e2b25ba336f9685ab9f27
Rasilgon <rasilgon@gmail.com>
2018-07-01 14:33:04 +0200

    Provided context to example

--- rLPJGUESS/inst/examples/writeTemplateHelp.R
+++ rLPJGUESS/inst/examples/writeTemplateHelp.R
@@ -1,4 +1,8 @@
 \dontrun{
-writeTemplate("global.ins", list(run_lamda_max = 0.5, run_emax= 5),
-               "/home/lpjRun/runDirectory1")
+# Define the parametes to be written
+parameterList <- list(run_lamda_max = 0.5, run_emax= 5)
+
+# write the template
+writeTemplate(template1 = "global.ins", parameterList = parameterList,
+              runDir = "/some/absolute/path/mainDir/runDirectory", check = "serial")
 }
